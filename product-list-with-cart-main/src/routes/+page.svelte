3f4614c7f1fdf1cffd5630cdabcaece6761ee368
CHEGEBB <chegephil24@gmail.com>
2024-11-12 14:59:06 +0300

    Refactor app.css and package.json files

--- product-list-with-cart-main/src/routes/+page.svelte
+++ product-list-with-cart-main/src/routes/+page.svelte
@@ -1,59 +1,27 @@
-<div class="container bg-green-600">
-
-
-    Desserts
-  
-    Waffle with Berries
-    Waffle
-    6.50
-    Add to Cart
-  
-    Vanilla Bean Crème Brûlée
-    Crème Brûlée
-    7.00
-    Add to Cart
-  
-    Macaron Mix of Five
-    Macaron
-    8.00
-    Add to Cart
-  
-    Classic Tiramisu
-    Tiramisu
-    5.50
-    Add to Cart
-  
-    Pistachio Baklava
-    Baklava
-    4.00
-    Add to Cart
-  
-    Lemon Meringue Pie
-    Pie
-    5.00
-    Add to Cart
-  
-    Red Velvet Cake
-    Cake
-    4.50
-    Add to Cart
-  
-    Salted Caramel Brownie
-    Brownie
-    4.50
-    Add to Cart
-  
-    Vanilla Panna Cotta
-    Panna Cotta
-    6.50
-    Add to Cart
-  
-    Your Cart (<!-- Quantity -->)
-    Your added items will appear here
-
+<div class="container bg-green-800">
+    <h1>Trial</h1>
+    Desserts Waffle with Berries Waffle 6.50 Add to Cart Vanilla Bean Crème Brûlée
+    Crème Brûlée 7.00 Add to Cart Macaron Mix of Five Macaron 8.00 Add to Cart Classic
+    Tiramisu Tiramisu 5.50 Add to Cart Pistachio Baklava Baklava 4.00 Add to Cart
+    Lemon Meringue Pie Pie 5.00 Add to Cart Red Velvet Cake Cake 4.50 Add to Cart
+    Salted Caramel Brownie Brownie 4.50 Add to Cart Vanilla Panna Cotta Panna Cotta
+    6.50 Add to Cart Your Cart (<!-- Quantity -->) Your added items will appear
+    here
 </div>
-<style>
-    *{
+
+<style lang="scss">
+    @import url("https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&family=Rubik:ital,wght@0,300..900;1,300..900&family=Sora:wght@100..800&display=swap");
+    $font-Red-Hat: "Red Hat Text", sans-serif;
+    $Red: hsl(14, 86%, 42%);
+    $Green: hsl(159, 69%, 38%);
+    $Rose50: hsl(20, 50%, 98%);
+    $Rose100: hsl(13, 31%, 94%);
+    $Rose300: hsl(14, 25%, 72%);
+    $Rose400: hsl(7, 20%, 60%);
+    $Rose500: hsl(12, 20%, 44%);
+    $Rose900: hsl(14, 65%, 9%);
+
+    * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
@@ -64,5 +32,6 @@
         align-items: center;
         height: 100vh;
         min-width: 100vw;
+        font-family: $font-Red-Hat;
     }
 </style>
