e23edd2c80eb4876ec2cfb8d823c2a85fa9a7a63
CHEGEBB <chegephil24@gmail.com>
2024-11-12 15:11:57 +0300

    Refactor product list and cart layout

--- product-list-with-cart-main/src/routes/+page.svelte
+++ product-list-with-cart-main/src/routes/+page.svelte
@@ -1,12 +1,15 @@
-<div class="container bg-green-800">
-    <h1>Trial</h1>
-    Desserts Waffle with Berries Waffle 6.50 Add to Cart Vanilla Bean Crème Brûlée
-    Crème Brûlée 7.00 Add to Cart Macaron Mix of Five Macaron 8.00 Add to Cart Classic
-    Tiramisu Tiramisu 5.50 Add to Cart Pistachio Baklava Baklava 4.00 Add to Cart
-    Lemon Meringue Pie Pie 5.00 Add to Cart Red Velvet Cake Cake 4.50 Add to Cart
-    Salted Caramel Brownie Brownie 4.50 Add to Cart Vanilla Panna Cotta Panna Cotta
-    6.50 Add to Cart Your Cart (<!-- Quantity -->) Your added items will appear
-    here
+<div class="container">
+    <div class="card-container bg-blue-600">
+        Desserts 
+        Waffle with Berries Waffle 6.50 Add to Cart Vanilla Bean Crème Brûlée
+        Crème Brûlée 7.00 Add to Cart Macaron Mix of Five Macaron 8.00 Add to Cart Classic
+        Tiramisu Tiramisu 5.50 Add to Cart Pistachio Baklava Baklava 4.00 Add to Cart
+        Lemon Meringue Pie Pie 5.00 Add to Cart Red Velvet Cake Cake 4.50 Add to Cart
+        Salted Caramel Brownie Brownie 4.50 Add to Cart Vanilla Panna Cotta Panna Cotta
+        6.50 Add to Cart Your Cart (<!-- Quantity -->) Your added items will appear
+        here    
+    </div>
+
 </div>
 
 <style lang="scss">
@@ -33,5 +36,6 @@
         height: 100vh;
         min-width: 100vw;
         font-family: $font-Red-Hat;
+        background:linear-gradient(90deg, $Rose100, $Rose50);
     }
 </style>
