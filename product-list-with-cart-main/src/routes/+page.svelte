b8ca9ffa7f14785e074a88aaee2a2786e7b47271
CHEGEBB <chegephil24@gmail.com>
2024-11-12 14:30:47 +0300

    initial commit

---
+++ product-list-with-cart-main/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="container bg-green-600">
+
+</div>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        min-width: 100vw;
+    }
+</style>
